507b43bb2ef6ca302af25aa1ca422c99f86551ad
Remi Cattiau <remi@cattiau.com>
2024-10-31 16:04:05 -0700

    fix: add bcc to nodemailer

--- src/processors/nodemailer.ts
+++ src/processors/nodemailer.ts
@@ -1,6 +1,7 @@
-import { ParsedMail } from "mailparser";
+import { AddressObject, ParsedMail } from "mailparser";
 import * as nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
+import AddressParser from "nodemailer/lib/addressparser";
 import { SmtpComponentConfig } from "../component";
 import { SmtpProcessor } from "../processor";
 import { mapAddressObjects, SmtpSession } from "../server";
@@ -29,10 +30,24 @@ export class NodeMailerProcessor<
    * @param email
    * @returns
    */
-  static transformEmail(email: ParsedMail): Mail.Options {
+  static transformEmail(session: SmtpSession): Mail.Options {
+    const email = session.email;
     const addressTransformer = a => {
       return a.value.map(ad => ad.address);
     };
+    // If no headers we do not fill the bcc as we have no to,cc headers
+    if (session.email.headerLines.length && !session.email.bcc) {
+      const noBcc = [session.email.cc, session.email.to].flat().filter(a => a);
+      const bcc: AddressObject[] = session.envelope.rcptTo
+        .filter(a => {
+          // Find address that are not included in the to/cc headers
+          return !noBcc.find(b => b.value.find(v => v.address === a.address));
+        })
+        .map(a => ({ value: AddressParser(a.address), text: a.address, html: a.address }));
+      if (bcc.length) {
+        session.email.bcc = bcc.length === 1 ? bcc.pop() : bcc;
+      }
+    }
     return {
       from: email.from.text,
       to: mapAddressObjects<string[]>(email.to, addressTransformer)?.flat(),
@@ -60,6 +75,6 @@ export class NodeMailerProcessor<
    * @returns
    */
   async onMail(session: SmtpSession): Promise<void> {
-    return this.transporter.sendMail({ ...NodeMailerProcessor.transformEmail(session.email), ...this.config.override });
+    return this.transporter.sendMail({ ...NodeMailerProcessor.transformEmail(session), ...this.config.override });
   }
 }
